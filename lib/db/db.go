e325559c468f0e26bd183a11ef1e424b009a6de9
Rikard Gynnerstedt <rikard@sinch.com>
2016-03-29 10:55:49 +0200

    added error checking for missing database path

--- lib/db/db.go
+++ lib/db/db.go
@@ -1,15 +1,43 @@
 package db
 
 import (
-    "github.com/jinzhu/gorm"
+  "os"
+  "log"
+  "github.com/joho/godotenv"
+  "github.com/jinzhu/gorm"
     _ "github.com/jinzhu/gorm/dialects/sqlite" //sqlite adapter
 )
 
+func databasePath() string {
+  err := godotenv.Load()
+  if err != nil {
+    log.Fatal("error loading .env file")
+  }
+  path := "./db/" + os.Getenv("MODE") + ".db"
+  return path
+}
+
+func exists(path string) (bool, error) {
+  _, err := os.Stat(path)
+  if err == nil { return true, nil }
+  if os.IsNotExist(err) { return false, nil }
+  return true, err
+}
+
 // OpenDBConnection opens a db connection
 func OpenDBConnection() *gorm.DB {
-    db, err := gorm.Open("sqlite3", "db/development.db")
+    path := databasePath()
+    pathExists, err := exists(path)
+    
+    if err != nil || pathExists == false {
+      log.Println("cannot find database folder, creating folder")
+      _ = os.Mkdir("db", os.FileMode(0777))      
+    }
+    
+    db, err := gorm.Open("sqlite3", path)
     if err != nil {
       panic("failed to connect database")
     }
+    
     return db
 }
